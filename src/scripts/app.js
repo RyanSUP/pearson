d16be3a9c9d45d6266323643f8f6ac0cf6d13967
RMo <morici23@gmail.com>
2022-07-13 15:53:59 -0400

    Cleanup app.js formatting.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,10 +1,10 @@
 $(function() {
     $( "#slider-range" ).slider({
-      range: true,
-      min: -5,
-      max: 5,
-      step: 1,
-      values: [ -2, 1 ],
+        range: true,
+        min: -5,
+        max: 5,
+        step: 1,
+        values: [ -2, 1 ],
     })
     .each(function() {
         // Reference post: https://stackoverflow.com/questions/10224856/jquery-ui-slider-labels-under-slider
@@ -13,19 +13,16 @@ $(function() {
         let vals = sliderOptions.max - sliderOptions.min;
 
         for (let i = 0; i <= vals; i++) {
-          let label = $('<label>'+(i-5)+'</label>');
-          label.css('left',(i/vals*100)+'%');
-          label.addClass('slider-label');
+            let label = $('<label>'+(i-5)+'</label>');
+            label.css('left',(i/vals*100)+'%');
+            label.addClass('slider-label');
 
-          let tick = $('<div></div>');
-          tick.css('left',(i/vals*100)+'%');
-          tick.addClass('tick');
+            let tick = $('<div></div>');
+            tick.css('left',(i/vals*100)+'%');
+            tick.addClass('tick');
 
-        
-          $( "#slider-range" ).append(label);
-          $( "#slider-range" ).append(tick);
-          
+            $( "#slider-range" ).append(label);
+            $( "#slider-range" ).append(tick);
         }
-
       });
   });
