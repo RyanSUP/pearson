95e4c76e3de063f91c208773a8e80f955132f733
RMo <morici23@gmail.com>
2022-07-13 15:42:58 -0400

    Style slider and ticks

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -7,25 +7,16 @@ $(function() {
       values: [ -2, 1 ],
     })
     .each(function() {
+        // Reference post: https://stackoverflow.com/questions/10224856/jquery-ui-slider-labels-under-slider
 
-        //
-        // Add labels to slider whose values 
-        // are specified by min, max and whose
-        // step is set to 1
-        //
-      
-        // Get the options for this slider
-        var opt = $(this).data().uiSlider.options;
-        console.log(opt)
-        // Get the number of possible values
-        var vals = opt.max - opt.min;
-        console.log(vals)
-        // Space out values
+        let sliderOptions = $(this).data().uiSlider.options;
+        let vals = sliderOptions.max - sliderOptions.min;
+
         for (var i = 0; i <= vals; i++) {
-          
           var el = $('<label>'+(i-5)+'</label>');
           el.css('position', 'absolute')
           el.css('left',(i/vals*100)+'%');
+          el.css('bottom','20px');
           $( "#slider-range" ).append(el);
           
         }
